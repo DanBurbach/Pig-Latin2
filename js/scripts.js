6e354734f4f43065505d4c6aec45b047cccfd924
Dan Burbach and Quinn Miller <quinn@>
2018-10-03 20:30:22 -0700

    edited JS to allow for the page to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
 // NOTE: business logic start
 $(document).ready(function() {
   var regex = /^[aeiou]/i;
-  var regex2 = /^[qwrtypsdfghjklzxcvbnm]*/i;
+  var regex2 = /^[qwrtpsdfghjklzxcvbnm]*/i;
+  var regex3 = /^qu/;
+  var regex4 = /^y/;
   function piglatin(words) {
     words.unshift("");
     for (i = 1; i < words.length; i++) {
-      if (regex.test(words[i])) {
-        words[i] = words[i].replace(words[i], words[i] + "way");
+        if (regex4.test(words[i])) {
+          var stringy = words[i].charAt(2);
+          var stringy2 = words[i].split(regex4, 2);
+          var stringy3 = stringy2[1]
+          var stringy4 = words[i].split(stringy, 1);
+          words[i] = stringy3 + stringy4 + "ay";
+      }
+        else if (regex3.test(words[i])) {
+          var stringqu = words[i].charAt(2);
+          var stringqu2 = words[i].split(regex3, 2);
+          var stringqu3 = stringqu2[1]
+          var stringqu4 = words[i].split(stringqu, 1);
+          words[i] = stringqu3 + stringqu4 + "ay";
+          console.log(stringqu);
+          console.log(stringqu2);
+          console.log(stringqu3);
+          console.log(stringqu4);
+      } else if (regex.test(words[i])) {
+          words[i] = words[i].replace(words[i], words[i] + "way");
       } else {
-        subarray2 = words[i].split(regex2);
-        substring = subarray2[1];
-        substring2 = substring.charAt(0);
-        subarray3 = words[i].split(substring2, 1);
-        words[i] = substring + subarray3 + "ay";
+          var subarray2 = words[i].split(regex2);
+          var substring = subarray2[1];
+          var substring2 = substring.charAt(0);
+          var subarray3 = words[i].split(substring2, 1);
+          words[i] = substring + subarray3 + "ay";
 
-        console.log(subarray2);
-        console.log(substring);
-        console.log(substring2);
-        console.log(subarray3);
+          console.log(subarray2);
+          console.log(substring);
+          console.log(substring2);
+          console.log(subarray3);
       }
     }
   };
